201b4a4a6230d1137b6f26bce9ddc0167b07f5f8
Cristopher <coronelcristopher553@gmail.com>
2023-07-10 16:05:37 -0500

    start commit

---
+++ src/main/java/com/volunteernet/volunteernet/VolunteernetApplication.java
@@ -0,0 +1,13 @@
+package com.volunteernet.volunteernet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolunteernetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VolunteernetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/volunteernet/volunteernet/VolunteernetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.volunteernet.volunteernet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VolunteernetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
