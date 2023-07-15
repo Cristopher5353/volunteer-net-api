e7b1411c39751212168c73756734927346e6ed0d
Cristopher <coronelcristopher553@gmail.com>
2023-07-15 08:44:14 -0500

    separate user detail service

---
+++ src/main/java/com/volunteernet/volunteernet/security/UserDetailService.java
@@ -0,0 +1,59 @@
+package com.volunteernet.volunteernet.security;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import com.volunteernet.volunteernet.models.Role;
+import com.volunteernet.volunteernet.models.User;
+
+public class UserDetailService implements UserDetails {
+
+    private User user;
+
+    public UserDetailService(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Role rol = user.getRole();
+        List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+
+        authorities.add(new SimpleGrantedAuthority(rol.getName()));
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+}
---
+++ src/main/java/com/volunteernet/volunteernet/security/UserDetailServiceImpl.java
@@ -0,0 +1,31 @@
+package com.volunteernet.volunteernet.security;
+
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import com.volunteernet.volunteernet.models.User;
+import com.volunteernet.volunteernet.repositories.IUserRepository;
+
+@Service
+public class UserDetailServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private IUserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> userByEmail = userRepository.findByEmail(username);
+
+        if (!userByEmail.isPresent()) {
+            throw new UsernameNotFoundException("Correo no existe en el sistema");
+        }
+
+        User user = userByEmail.orElseThrow();
+
+        return new UserDetailService(user);
+    }
+
+}
